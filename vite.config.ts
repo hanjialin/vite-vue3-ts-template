fee590ce39fc1ea1e5f194d2d9c44eb857f12484
hanjialin <hanjialin1991@hotmail.com>
2022-11-02 10:22:09 +0800

    fixed bug

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig, loadEnv } from 'vite'
+import VitePluginHtmlEnv from 'vite-plugin-html-env'
 import vue from '@vitejs/plugin-vue'
-import { createHtmlPlugin } from 'vite-plugin-html'
 // @ts-ignore
 import path from 'path'
 // import viteCompression from 'vite-plugin-compression'
@@ -10,26 +10,8 @@ export default (mode: string) =>
     base: './',
     plugins: [
       vue(),
-      createHtmlPlugin({
-        minify: true,
-        inject: {
-          data: {
-            title: loadEnv(mode, process.cwd()).VITE_APP_TITLE
-          }
-        }
-        /**
-         * 在这里写entry后，你将不需要在`index.html`内添加 script 标签，原有标签需要删除
-         * @default src/main.ts
-         */
-        //entry: 'src/main.ts',
-        /**
-         * 如果你想将 `index.html`存放在指定文件夹，可以修改它，否则不需要配置
-         * @default index.html
-         */
-        //template: 'public/index.html',
-        /**
-         * 需要注入 index.html ejs 模版的数据
-         */
+      VitePluginHtmlEnv({
+        compiler: true
       })
       /*viteCompression({
       verbose: true,
